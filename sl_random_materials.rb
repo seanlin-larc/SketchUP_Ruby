e7f63a760b4126088db0702ec9dbff015515f7fc
seanlin-larc <seanlin.larc@gmail.com>
2016-12-23 15:33:34 -0800

    Update sl_random_materials.rb

--- sl_random_materials.rb
+++ sl_random_materials.rb
@@ -23,44 +23,44 @@ WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED WARRANTIES OF
 MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
 =end
 
-module SL # <--Change this to your own personal name space
+module SL
+	module Extensions
+		module SL_Random_Materials
 
-	module SL_Random_Materials # <--Change this to your own scripts name space
-
-		### CONSTANTS ###
-		# Extension Information
-		EXTENSION		= self
-		EXTENSION_ID	= 'SL_Random_Materials'.freeze
-		EXTENSION_NAME	= 'SL Randomly Apply Materials'.freeze
+			### CONSTANTS ###
+			# Extension Information
+			EXTENSION		= self
+			EXTENSION_ID	= 'SL_Random_Materials'.freeze
+			EXTENSION_NAME	= 'SL Randomly Apply Materials'.freeze
 		
-		# Resource paths
-		file = File.expand_path ( _FILE_ )
-		file.force_encoding ("UTF-8) if file.respond_to? ( :force_encoding )
-		FILENAMESPCAE	= File.basename( file, '.*')
-		PATH_ROOT		= File.dirname( file ).freeze
-		PATH			= File.join( PATH_ROOT, FILENAMESPACE ).freeze
+			# Resource paths
+			file = File.expand_path ( _FILE_ )
+			file.force_encoding ("UTF-8) if file.respond_to? ( :force_encoding )
+			FILENAMESPCAE	= File.basename( file, '.*')
+			PATH_ROOT		= File.dirname( file ).freeze
+			PATH			= File.join( PATH_ROOT, FILENAMESPACE ).freeze
 
-		# Common required files for creating extensions
-		require 'sketchup.rb'
-		require 'extensions.rb'
-		require 'langhandler.rb'
+			# Common required files for creating extensions
+			require 'sketchup.rb'
+			require 'extensions.rb'
+			require 'langhandler.rb'
 
-		### MENU ###
-		# The "new" method is used to create a new SketchupExtension object. 
-		# Note that once the extension object is created, it will not appear in 
-    	# the Extension Manager dialog until your register it with the 
-    	# Sketchup.register_extension method.
+			### MENU ###
+			# The "new" method is used to create a new SketchupExtension object. 
+			# Note that once the extension object is created, it will not appear in 
+    		# the Extension Manager dialog until your register it with the 
+    		# Sketchup.register_extension method.
 		
-		unless file_loaded?( _FILE_ )
-			file = File.join( PATH, 'sl_random_materials_core.rb')
-			extn = SketchExtension.new( EXTENSION_NAME, file)
-			extn.description = "Randomly apply materials to faces by assigned propotion"
-			extn.version = '1.0.0'
-			extn.copyright = 'Sean Lin @2017'
-			extn.creator = 'Sean Lin (seanlinlarc@gmail.com)'
-			Sketchup.register_extension(extn, true)
-		end
+			unless file_loaded?( _FILE_ )
+				file = File.join( PATH, 'sl_random_materials_core.rb')
+				extn = SketchExtension.new( EXTENSION_NAME, file)
+				extn.description = "Randomly apply materials to faces by assigned propotion"
+				extn.version = '1.0.0'
+					extn.copyright = 'Sean Lin @2017'
+				extn.creator = 'Sean Lin (seanlinlarc@gmail.com)'
+				Sketchup.register_extension(extn, true)
+			end
 		
-	end # module SL_Random_Materials
-
+		end # module SL_Random_Materials
+	end # module Extensions
 end # module SL
