6f898da016b426e63f5eacb105ef5f7eefaf2f8d
seanlin-larc <seanlin.larc@gmail.com>
2017-01-03 12:16:59 -0800

    Update sl_random_materials_core.rb

--- sl_random_materials/sl_random_materials_core.rb
+++ sl_random_materials/sl_random_materials_core.rb
@@ -1,7 +1,6 @@
 =begin
 Copyright 2017 Sean Lin
 All Rights Reserved
-
 Disclaimer
 THIS SOFTWARE IS PROVIDED "AS IS" AND WITHOUT ANY EXPRESS OR IMPLIED 
 WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED WARRANTIES OF 
@@ -29,6 +28,9 @@ module SL::Extensions::SL_Random_Materials
 		model = Sketchup.active_model
 		sel = model.selection
 		ents = model.active_entities
+	
+		#Create an Undo point
+		model.start_opperation('SL Random Materials', true)	
 		
 		#Leave only faces in the selection
 		sel_faces = []
@@ -37,6 +39,7 @@ module SL::Extensions::SL_Random_Materials
 				sel_faces << sel[i]
 			end
 			}
+		
 		#Extract materials from selected faces
 		sel_mats = []
 		sel_faces.each {|i|
@@ -44,7 +47,7 @@ module SL::Extensions::SL_Random_Materials
 			sel_mats << mat
 			}
 		sel_mats.uniq! #Keep only unique materials
-		sel_mats.delete(nil) #Remove "Default" material.
+		sel_mats.compact! #Remove "Default" material.
 		
 			
 		#Check number of materials
@@ -56,13 +59,15 @@ module SL::Extensions::SL_Random_Materials
 			#Confirm number of materials
 			confirm = UI.messagebox("#{sel_mats.length} materials are selected. \nOK to continue; Cancel to Abort.", type = MB_OKCANCEL) 	
 			break unless confirm == 1
-		end
+		end #if sel_mats.length
 				
 		#Assign seeds to materials
 		ary_mats_seeds = []
 		tot_seeds = 0
 		mats_pct_desc = ""
+		
 		sel_mats.each {|i|
+			mat_id = sel_mats[i]
 			mat_name = sel_mats[i].name
 			UI.messagebox("Proportion of a material is the seed of material 
 				\ndivided by the total seeds.
@@ -72,17 +77,17 @@ module SL::Extensions::SL_Random_Materials
 			default = [0]
 			mat_seed = UI.inputbox(prompt, default, "Input Number of Seeds for material #{mat_name}").at(0)
 			tot_seeds += mat_seed
-			hash_mat_seed = {:mat_name => mat_name, :mat_seed => mat_seed}
+			hash_mat_seed = {:mat_id => mat_id, :mat_name => mat_name, :mat_seed => mat_seed, :mat_accum_seed => tot_seeds}
 			ary_mats_seeds << hash_mat_seed
-			}
+			} #End sel_mats.each
 		
 		#Calculate proportion
 		ary_mats_seeds.each {|i|
-			hash_mat_seed_pct = (ary_mats_seeds[i].to_f / tot_seeds.to_f).round(4) * 100
+			hash_mat_seed_pct = (ary_mats_seeds[i].to_f / tot_seeds.to_f).round(4) * 100)
 			ary_mats_seeds[i][:mat_pct] = hash_mat_seed_pct
 			#Make a description
 			mats_pct_desc += "#{ary_mats_seeds[i][:mat_name]} will take #{ary_mats_seeds[i][:mat_pct]} %. \n"
-			}
+			} #Enad ary_mats_seeds.each
 		
 		#Display materials and their proportions
 		confirm = UI.messagebox("#{mats_pct_desc} \n OK to continue. \n Cancel to abort.", type = MB_OKCANCEL)
@@ -90,58 +95,23 @@ module SL::Extensions::SL_Random_Materials
 		
 		
 #Assigning materials to faces
-#Refer to Alternative Process below
 		
-		#build accumulate seed counts
-		acc_seed_counts = []
-		ary_mats_seeds.each {|i|
-			sum = acc_counts.reduce(:+) 
-			acc_counts << ary_mats_seeds[i][:mat_seed]+ sum
-			}
-==>		sel.faces.each {|i|
-			}
+		sel_faces.each {|i|
+			agnt = rand(tot_seeds)
+			ary_mats_seeds.each {|m| 
+				retrn = agnt <= ary_mats_seeds[m][:mat_accum_seed]
+				if return = true
+				sel_faces[i].material = ary_mats_seeds[m][:mat_id]
+				end #if
+			} #end ary_mats_seeds.each
+		} #end sel_faces.each
 		
-		
-#Alternative Process: Ask users to pick faces		
-#Discarded cause limited knowledge to Sketchup API
-=begin
+		#Mark the end of Undo point
+		model.commit_opperation	
 
-		
-		#Ask for faces to be applied
-		sel.clear
-		sel.faces.clear
-		until sel.faces.length != 0 do
-			UI.messagebox("Cross-window-picking to select faces to be applied....", type = MB_OK)
-			view = model.active_view
-			pick_helper = view.pick_helper
-			start_point = view.Sketchup::Inputpoint.new
-			end_point = view.Sketchup::Inputpoint.new
-			sel = pick_helper.window_pick(start_point, end_point, Sketchup::Pick_helper::PICK_CROSSING)
-		
-			#Leave only faces in the selection
-			sel.each {|i|
-				if sel[i].class == Sketchup::Face
-					sel_faces << sel[i]
-				end
-				}
-			#Check number of faces
-			if sel_faces.length == 0
-				prompt = "Need more than one face to run the script. \n OK to re-select. \nCancel to abort."
-				confirm = UI.messagebox(prompt, type = MB_OKCANCEL)
-				if confirm == 2
-					break
-				else
-				end
-			else	
-			end
-		end #until 
-			#Apply materials to faces
-=end
-		
-		end
-
-		
+		#Set current tool to "Selection Tool"
+		Sketchup.send_action "selectSelectionTool:"
+	
 	end # cmd	
  
 end #module
-
